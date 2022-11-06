8baa3cb973621837f602d7bbac220de431a017c5
Danilo Serafim <danilob.serafim@gmail.com>
2022-11-06 10:32:15 -0300

    melhoria no ui

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -14,17 +14,21 @@ async function ShowPokemons() {
     menuPage.style.color = 'gray'
     menuPage.href = '#'
     
+    const pokemons = []
     for (let i = 0; i < 60; i++) {
         
-        var poke = await fetch(`https://pokeapi.co/api/v2/pokemon/${(thisPage - 1) * 66 + i + 1 }/`).then(response => response.json()).then(data => data)
+        pokemons[i] = await fetch(`https://pokeapi.co/api/v2/pokemon/${(thisPage - 1) * 66 + i + 1 }/`).then(response => response.json()).then(data => data)
 
-        cards.innerHTML += `<li class='card ${poke.types[0].type.name}' onClick='ShowDetails(${poke.id})'>
+        
+    }  
+    pokemons.map(pokemon =>{
+        cards.innerHTML += `<li class='card ${pokemon.types[0].type.name}' onClick='ShowDetails(${pokemon.id})'>
         <div class='containerTypes'>
-        ${poke.types.map( typeName => `<span class='type'>${typeName.type.name}</span>`)}
+        ${pokemon.types.map( typeName => `<span class='type'>${typeName.type.name}</span>`)}
         </div>
-        <img src=${poke.sprites.other.dream_world.front_default ? poke.sprites.other.dream_world.front_default : "./assets/images/no_foto.png"} class='cardImg'/>
-        <h1 class='pokeName'>${poke.name}</h1>
+        <img src=${pokemon.sprites.other.dream_world.front_default ? pokemon.sprites.other.dream_world.front_default : "./assets/images/no_foto.png"} class='cardImg'/>
+        <h1 class='pokeName'>${pokemon.name}</h1>
         </li>
         `
-    }  
+    })
 }
